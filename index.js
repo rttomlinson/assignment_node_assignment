aa942a299d2a3c604cb846f76a43ec3d0291f15e
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-12 12:41:02 +0000

    Add logger.js modules in lib/

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var chalk = require("chalk");
 var _ = require("lodash");
+var logger = require("./lib/logger.js");
 
+/* Demonstration Purposes
 console.log(chalk.blue("Hello world!"));
 var arr = ["a", "b", "c"];
 
 _.each(arr, function(element, index, collection) {
     console.log("Value at index", chalk.green(index), "is", chalk.red(element));
 });
+*/
+
+const LEVELS = {
+    "INFO" : chalk.blue,
+    "WARNING" : chalk.yellow,
+    "ERROR" : chalk.red,
+};
+
+function logMessage(message, level) {
+    logger.log(LEVELS[level](message));
+}
+
+logMessage("INFO", "INFO");
+logMessage("WARNING", "WARNING");
+logMessage("ERROR", "ERROR");
