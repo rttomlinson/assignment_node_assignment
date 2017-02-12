a23d3bdeb04e7980685beb2fe9b92879ac38b82f
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-12 12:20:51 +0000

    Test chalk and lodash modules

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+var chalk = require("chalk");
+var _ = require("lodash");
+
+console.log(chalk.blue("Hello world!"));
+var arr = ["a", "b", "c"];
+
+_.each(arr, function(element, index, collection) {
+    console.log("Value at index", chalk.green(index), "is", chalk.red(element));
+});
